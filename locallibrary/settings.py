36b86f79fc5779fcaeb779716680ed4e1e81bfb3
jeffy-sajan <jeffysajan9400@gmail.com>
2025-03-17 14:55:11 +0530

    lowky commit

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -136,6 +136,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
